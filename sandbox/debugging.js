62a8895bffb4b07d0f28c38aa688b985f7374c3d
artemisiaporto <artemisiaporto@outlook.com>
2023-09-16 23:52:39 -0300

    Reorganizing the folders

---
+++ sandbox/debugging.js
@@ -0,0 +1,15 @@
+const PI = 3.14;
+let radius = 3;
+let area = 0;
+function circleArea(radius) {
+    //code to complete our task here
+    const area = radius * PI;
+    return area;
+}
+area = circleArea(3);
+console.log(area);
+
+let num1 = 100;
+let num2 = '10';
+let sum = num1 + parseInt(num2);
+console.log(sum)
